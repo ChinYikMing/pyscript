44af7949fd8e738e80fc7ea31d9940e96eed5506
ChinYikMing <yikming2222@gmail.com>
2021-08-09 02:13:51 +0800

    slice

---
+++ slice.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+text = "Learn Python Script"
+
+slice_text = slice(5)
+print(slice_text)
+print(text[slice_text])
+
+slice_text2 = slice(0, 12)
+print(slice_text2)
+print(text[slice_text2])
