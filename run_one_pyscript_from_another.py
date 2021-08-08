186e2e6b831889447581f596013f7775d5b522e9
ChinYikMing <yikming2222@gmail.com>
2021-08-09 01:53:17 +0800

    import other pyscript and run

---
+++ run_one_pyscript_from_another.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import hello_world as hw
+import symbols as sym
+
+hw
+
+print("symbol1: %s" % sym.symbol1)
+print("symbol2: %s" % sym.symbol2)
+print("symbol3: %s" % sym.symbol3)
---
+++ symbols.py
@@ -0,0 +1,3 @@
+symbol1 = "sym1"
+symbol2 = "sym2"
+symbol3 = "sym3"
