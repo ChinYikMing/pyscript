fd9b3a6671b1cf29ecb2b299a7c533c1264874fc
ChinYikMing <yikming2222@gmail.com>
2021-08-09 01:49:49 +0800

    for loop

---
+++ for.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+i = 0
+while(i != 100):
+    print("i: %d" % i)
+    i += 1
+
+weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+for day in weekdays:
+    print("%s" % day)
