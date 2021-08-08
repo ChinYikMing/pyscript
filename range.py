18454bf38b38ebd8bf36cbab98c03aeaf604b79c
ChinYikMing <yikming2222@gmail.com>
2021-08-09 02:42:42 +0800

    range

---
+++ range.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+print("first")
+for i in range(6):
+    print(i)
+
+print("\nsecond")
+for i in range(6, 10):
+    print(i)
+
+print("\nthird")
+for i in range(6, 10, 2):
+    print(i)
