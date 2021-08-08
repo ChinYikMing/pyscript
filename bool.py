cac6d3c3faca50e7095895dd6388840c32dfde35
ChinYikMing <yikming2222@gmail.com>
2021-08-09 01:32:24 +0800

    boolean

---
+++ bool.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+val1 = True
+print(val1)
+
+number = 10
+print(bool(number))
+
+number = -5
+print(bool(number))
+
+number = 0
+print(bool(number))
+
+val1 = 5
+val2 = 2
+print(val1 < val2)
+print(val1 > val2)
