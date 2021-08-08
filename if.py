82043c052f63529bebdcae8455bb0acc65b0683f
ChinYikMing <yikming2222@gmail.com>
2021-08-09 01:35:33 +0800

    if statement

---
+++ if.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+number = 520
+
+if(number > 520):
+    print("%d is greater than 520" % number)
+elif(number == 520):
+    print("%d is equal than 520" % number)
+else:
+    print("%d is smaller than 520" % number)
+
+number = 519
+
+if(number > 520):
+    print("%d is greater than 520" % number)
+elif(number == 520):
+    print("%d is equal than 520" % number)
+else:
+    print("%d is smaller than 520" % number)
+
+number = 521
+
+if(number > 520):
+    print("%d is greater than 520" % number)
+elif(number == 520):
+    print("%d is equal than 520" % number)
+else:
+    print("%d is smaller than 520" % number)
