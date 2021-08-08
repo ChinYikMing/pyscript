cb60016e4edb8425eba26eda7d2d11071b308d53
ChinYikMing <yikming2222@gmail.com>
2021-08-09 02:10:53 +0800

    list comprehension

---
+++ list_rep.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+char_list = [char for char in "linux"]
+for c in char_list:
+    print(c)
+
+websites = ("google..com", "yahoo.com", "ask.com")
+site_list = [site for site in websites]
+for s in site_list:
+    print(s)
